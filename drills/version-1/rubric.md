12196470cc5f548120ded9b97f6724d71c60d4d9
Kyle Coberly <kyle.coberly@gmail.com>
2017-11-06 22:33:35 -0700

    Revising instructions

--- drills/version-1/rubric.md
+++ drills/version-1/rubric.md
@@ -1,1 +1,8 @@
-- [ ] Tests pass
+All items required to pass
+
+* [ ] 3 passing tests
+* [ ] `candySalesToObject` uses `reduce`
+* [ ] `salesDayToObject` uses `reduce`
+* [ ] `allSalesToArray` uses `map`
+* [ ] Meaningful variable names
+* [ ] Proper indentation of all code
--- drills/version-1/test/exercise-spec.js
+++ drills/version-1/test/exercise-spec.js
@@ -1,57 +1,87 @@
-var assert = require('assert')
-var exercise = require('../src/exercise')
+var assert = require("assert")
+var exercise = require("../exercise")
 
-describe('Version-1 Exercises', function () {
-  describe('#accessesingData1()', function () {
-    it('returns \'4.63\'', function () {
-      assert.equal(exercise.accessesingData1(), '4.63')
-    })
-  })
-  describe('#accessesingData2()', function () {
-    it('returns 3', function () {
-      assert.equal(exercise.accessesingData2(), 3)
-    })
-  })
-  describe('#loopingData1()', function () {
-    it('returns array of candy names', function () {
-      var result = ['Banana Bunches', 'Peppermint Poppers', 'Mint Wafers', 'Dark Chocolate Crunchies', 'Caramel Twists']
-      assert.deepEqual(exercise.loopingData1(), result)
-    })
-  })
-  describe('#loopingData2()', function () {
-    it('returns 22', function () {
-      assert.equal(exercise.loopingData2(), 22)
-    })
-  })
-  describe('#loopingData3()', function () {
-    it('returns array of date strings', function () {
-      var dates = [
-        '2015-01-06',
-        '2015-01-07',
-        '2015-01-08',
-        '2015-01-09',
-        '2015-01-10'
-      ]
-      var result = exercise.loopingData3()
-      assert.equal(result.length, dates.length)
-      result.forEach(function (date) {
-        assert.notEqual(dates.indexOf(date), -1)
-      })
-    })
-  })
-  describe('#loopingData4()', function () {
-    it('returns total number of items sold', function () {
-      assert.equal(exercise.loopingData4(), 68)
-    })
-  })
-  describe('#loopingData5()', function () {
-    it('returns a string of your answer', function () {
-      assert.notEqual(exercise.loopingData5(), 'Answer')
-    })
-  })
-  describe('#challenge1()', function () {
-    it('returns a number of total revenue, rounded to two numbers', function () {
-      assert.deepEqual(exercise.challenge1(), 75.90)
-    })
-  })
-})
+describe("Version-1 Exercises", ()=>{
+    describe("#candySalesToObject", ()=>{
+        it("maps an array of candy sales to a labeled object", ()=>{
+            assert.deepEqual(exercise.candySalesToObject(["Dark Chocolate Crunchies", "4.39", 1]), {
+                item: "Dark Chocolate Crunchies",
+                price: 4.39,
+                quantity: 1
+            });
+            assert.deepEqual(exercise.candySalesToObject(["a", "3", 2]), {
+                item: "a",
+                price: 3,
+                quantity: 2
+            });
+        });
+    });
+    describe("#salesDayToObject", ()=>{
+        it("maps an array of sales days to a labeled object", ()=>{
+            assert.deepEqual(exercise.salesDayToObject("2015-01-06", {
+                "2015-01-06": [
+                    ["Dark Chocolate Crunchies", "4.39", 1],
+                    ["Mint Wafers", "1.19", 3]
+                ]
+            }), {
+                date: "2015-01-06",
+                sales: [{
+                    item: "Dark Chocolate Crunchies",
+                    price: 4.39,
+                    quantity: 1
+                },{
+                    item: "Mint Wafers",
+                    price: 1.19,
+                    quantity: 3
+                }]
+            });
+            assert.deepEqual(exercise.salesDayToObject("a", {
+                "a": [["a", "2", 2],["b", "1", 1]]
+            }), {
+                date: "a",
+                sales: [{
+                    item: "a",
+                    price: 2,
+                    quantity: 2
+                },{
+                    item: "b",
+                    price: 1,
+                    quantity: 1
+                }]
+            });
+        });
+    });
+    describe("#allSalesToArray", ()=>{
+        const allSales = {
+          "2015-01-06": [
+                ["Dark Chocolate Crunchies", "4.39", 1],
+                ["Mint Wafers", "1.19", 3]
+          ],
+          "2015-01-07": [
+                ["Dark Chocolate Crunchies", "4.39", 4]
+          ]
+        };
+        const allSalesArray = [{
+            date: "2015-01-06",
+            sales: [{
+                item: "Dark Chocolate Crunchies",
+                price: 4.39,
+                quantity: 1
+            },{
+                item: "Mint Wafers",
+                price: 1.19,
+                quantity: 3
+            }]
+        },{
+            date: "2015-01-07",
+            sales: [{
+                item: "Dark Chocolate Crunchies",
+                price: 4.39,
+                quantity: 4
+            }]
+        }];
+        it("converts a sales object to an array of sales objects", ()=>{
+            assert.deepEqual(exercise.allSalesToArray(allSales), allSalesArray);
+        });
+    });
+});
