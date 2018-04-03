46cde8e13a0f079aa28cc391f0d69afff85e63e3
Allison Livingston <livingstona16@gmail.com>
2018-04-03 10:47:18 -0600

    First test passing

--- drills/version-1/exercise.js
+++ drills/version-1/exercise.js
@@ -1,6 +1,56 @@
+
+// let salesObject = {
+//   item: "",
+//   price: "",
+//   quantity: ""
+// }
+
+function candySalesToObject(array) {
+  let priceToNumber = parseFloat(array[1])
+
+  return array.reduce(function(accumulator, currentValue, index) {
+    switch(index) {
+      case 0:
+        accumulator.item = currentValue
+        break
+      case 1:
+        accumulator.price = currentValue
+        break
+      case 2:
+        accumulator.quantity = currentValue
+        break
+    }
+    return accumulator
+  }, {})
+}
+
+
+
+
+  //
+  // array.map(function(item) {
+  //   return salesObject.item = array[0]
+  // })
+  // array.map(function(item) {
+  //   // return salesObject.item = array[0]
+  //   return salesObject.price = priceToNumber
+  //   // salesObject.quantity = array[2]
+  // })
+  // array.map(function(item) {
+  //   // return salesObject.item = array[0]
+  //   // return salesObject.price = priceToNumber
+  //   return salesObject.quantity = array[2]
+  // })
+  // console.log(salesObject)
+
+function salesDayToObject(date, day) {
+  return
+}
+
+
+
 module.exports = {
-    // Uncomment these functions as you write them
-    // candySalesToObject,
-    // salesDayToObject,
+candySalesToObject,
+    salesDayToObject,
     // allSalesToArray
 };
