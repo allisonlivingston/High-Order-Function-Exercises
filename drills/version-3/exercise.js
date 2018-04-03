89f5d1ff275585f92501e095dbab1b51598b0e53
Allison Livingston <livingstona16@gmail.com>
2018-04-03 16:23:12 -0600

    Adding finished files

--- drills/version-3/exercise.js
+++ drills/version-3/exercise.js
@@ -1,6 +1,29 @@
+function getTotalItemSales(sales, id) {
+  let filteredSalesArray = sales.filter(function(object) {
+    return object.itemId == id
+  })
+
+  return filteredSalesArray.reduce(function(totalQuantity, currentQuantity){
+    return totalQuantity += currentQuantity.quantity
+  }, 0)
+}
+
+function addTotalSalesToItems(sales, items) {
+  return items.map(function(getItem) {
+    getItem.quantity = getTotalItemSales(sales, getItem.id)
+    return getItem
+  })
+}
+
+function addTotalValueToItems(sales, items) {
+  return addTotalSalesToItems(sales, items).map(function(totalCost) {
+      totalCost.totalValue = (totalCost.price * totalCost.quantity).toFixed(2)
+      return totalCost
+  })
+}
+
 module.exports = {
-    // Uncomment these functions as you write them
-    // getTotalItemSales,
-    // addTotalSalesToItems,
-    // addTotalValueToItems
+    getTotalItemSales,
+    addTotalSalesToItems,
+    addTotalValueToItems
 }
