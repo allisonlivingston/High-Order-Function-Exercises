b8cbd75edcc3f2d83cfa1e88b2664d600959f941
Allison Livingston <livingstona16@gmail.com>
2018-04-03 15:30:29 -0600

    Adding final files

--- drills/version-2/exercise.js
+++ drills/version-2/exercise.js
@@ -1,6 +1,28 @@
+function getItemById(items, itemId) {
+
+  return items.filter(function(object) {
+    return object.id == itemId
+  }) [0]
+}
+
+function buildTransactions(sales, items) {
+  return sales.map(function(transaction) {
+  let newSalesArray = Object.assign(transaction, getItemById(items, transaction.itemId))
+
+  delete newSalesArray.id
+  return newSalesArray
+  })
+  console.log(sales);
+}
+
+function getTransactionsByItemDescription(transactions, description) {
+  return transactions.filter(function(sales) {
+    return sales.description == description
+  })
+}
+
 module.exports = {
-    // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
+    getItemById,
+    buildTransactions,
+    getTransactionsByItemDescription
 }
