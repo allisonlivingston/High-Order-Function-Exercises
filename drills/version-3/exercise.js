c0ee41902ede727710e2622260b2dcebf493aa34
Allison Livingston <livingstona16@gmail.com>
2018-04-04 11:40:20 -0600

    Adding completed JS file and screen shot

--- drills/version-3/exercise.js
+++ drills/version-3/exercise.js
@@ -1,24 +1,23 @@
 function getTotalItemSales(sales, id) {
-  let filteredSalesArray = sales.filter(function(object) {
+  let filteredSalesArray = sales.filter(object => {
     return object.itemId == id
   })
-
-  return filteredSalesArray.reduce(function(totalQuantity, currentQuantity){
+  return filteredSalesArray.reduce((totalQuantity, currentQuantity) => {
     return totalQuantity += currentQuantity.quantity
   }, 0)
 }
 
 function addTotalSalesToItems(sales, items) {
-  return items.map(function(getItem) {
+  return items.map(getItem => {
     getItem.quantity = getTotalItemSales(sales, getItem.id)
     return getItem
   })
 }
 
 function addTotalValueToItems(sales, items) {
-  return addTotalSalesToItems(sales, items).map(function(totalCost) {
-      totalCost.totalValue = (totalCost.price * totalCost.quantity).toFixed(2)
-      return totalCost
+  return addTotalSalesToItems(sales, items).map(totalCost => {
+    totalCost.totalValue = (totalCost.price * totalCost.quantity).toFixed(2)
+    return totalCost
   })
 }
 
